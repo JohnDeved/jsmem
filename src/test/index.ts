3afb8656099f70e811450a8eb7a77edb80fc7bc3
Undefined <johann@objekt.stream>
2019-04-05 11:07:51 +0200

    + update test example

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,25 +1,26 @@
 import { jsmem } from '../jsmem'
 
-// const proc = jsmem.getProcess("DyingLightGame.exe")
-// const game = jsmem.process(proc)
-// const dll = game.getModule('gamedll_x64_rwdi.dll')
+const proc = jsmem.getProcess("DyingLightGame.exe")
+const game = jsmem.process(proc)
+const dll = game.getModule('gamedll_x64_rwdi.dll')
 
-// const damageAdrr = dll.modBaseAddr + 0xAD2D37
-// const damageComp = Buffer.from([0xF3, 0x0F, 0x11, 0x83, 0x54, 0x09, 0x00, 0x00])
-// const damageNop = Buffer.from([0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90])
-// const damageGame = game.getBuffer(damageAdrr, 8)
+const damageAdrr = dll.modBaseAddr + 0xAD2D37
+const damageComp = Buffer.from([0xF3, 0x0F, 0x11, 0x83, 0x54, 0x09, 0x00, 0x00])
+const damageNop = Buffer.from([0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90])
 
-// if (Buffer.compare(damageGame, damageComp) === 0) {
+const damageGame = game.memory[damageAdrr].buffer[8]
 
-//   // opcode is as expected
-//   game.setBuffer(damageAdrr, damageNop) // godmode!
+if (Buffer.compare(damageGame, damageComp) === 0) {
 
-// } else if (Buffer.compare(damageGame, damageNop) === 0) {
+  // opcode is as expected
+  game.memory[damageAdrr].buffer[8] = damageNop // godmode!
 
-//   // opcode has been modifyed
-//   game.setBuffer(damageAdrr, damageComp) // disable godmode
+} else if (Buffer.compare(damageGame, damageNop) === 0) {
 
-// }
+  // opcode has been modifyed
+  game.memory[damageAdrr].buffer[8] = damageComp // disable godmode
+
+}
 
 // 2nd game
 const process = jsmem.getProcess('Sakura Clicker.exe')
